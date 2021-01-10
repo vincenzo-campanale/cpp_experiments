dd63973b7aa9b1c39364be7f15c0082d17e2ddcd
Vincenzo Campanale <vinc.campanale@gmail.com>
2021-01-10 11:23:56 +0100

    Use decltype instead of type traits for identifing return type

--- bind2nd/src/bind2nd.h
+++ bind2nd/src/bind2nd.h
@@ -1,7 +1,6 @@
 #ifndef BIND2ND_H
 #define BIND2ND_H
 
-#include <type_traits>
 #include <utility>
 
 namespace detail
@@ -10,15 +9,15 @@ namespace detail
 template<typename F, typename T>
 class Binder2nd
 {
+  private:
+    F f_;   //< The binary function is stored within this wrapper
+    T y_;   //< together with its 2nd argument binded.
+
   public:
     Binder2nd(F f, T y);
 
     template<typename P>
-    auto operator()(P&& x) const -> std::result_of_t<F(P, T)>;
-
-  private:
-    F f_;
-    T y_;   //< The 2nd argument of f() is stored within this wrapper and used within the call operator().
+    auto operator()(P&& x) const -> decltype(f_(std::forward<P>(x), y_)); //< Unary call operator.
 };
 
 template<typename F, typename T>
@@ -30,17 +29,21 @@ Binder2nd<F, T>::Binder2nd(F f, T y)
 
 template<typename F, typename T>
 template<typename P>
-auto Binder2nd<F, T>::operator()(P&& x) const -> std::result_of_t<F(P, T)>
+auto Binder2nd<F, T>::operator()(P&& x) const -> decltype(f_(std::forward<P>(x), y_))
 {
     return f_(std::forward<P>(x), y_);
 }
 
 }  // namespace detail
 
-///
-///@brief Binds a given argument y to a second parameter of the given binary function object f. That is, stores y within
-///       the resulting wrapper, which, if called, passes y as the second parameter of f.
-///
+// Binds a given argument y to a second parameter of the given binary function object f. That is, stores y within the
+// resulting wrapper, which, if called, passes y as the second parameter of f.
+//
+// Note: this function template, whose only job is to make an instance of Binder2nd, allows the compiler to deduce the
+//       types automatically (template argument deduction is currently supported for functions but not for classes).
+// Clarification: the requirement of class template instantiation for the template arguments to be explicit defined has
+//                been removed in C++17. However this standard hasn't been yet widely accepted, this example doesn't
+//                rely on this feature.
 template<typename F, typename T>
 auto Bind2nd(F&& f, T&& y) -> detail::Binder2nd<F, T>
 {
--- bind2nd/src/main.cpp
+++ bind2nd/src/main.cpp
@@ -1,34 +1,34 @@
 #include "bind2nd.h"
 #include <algorithm>
-#include <iostream>
 #include <functional>
+#include <iostream>
+#include <string>
 #include <vector>
 
 template<typename Collection>
-void Print(const Collection& collection)
+void Print(const std::string& prefix, const Collection& collection)
 {
+   std::cout << prefix;
+
    std::for_each(std::begin(collection), std::end(collection), [](const auto& item){
       std::cout << item << " ";
    });
+
+   std::cout << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
    std::vector<int> numbers{12, 87, 54, 62, 4, 21, 44};
 
-   std::cout << "Before partitioning: ";
-   Print(numbers);
-   std::cout << std::endl;
+   Print("Before partitioning: ", numbers);
 
-   // Create a new unary function from the binary function std::greater where the second argument is 42.
+   // Create an unary function from the binary function std::greater with the second argument binded to 42 and use it
+   // within the std::partition algorithm.
    auto greater_than_42{Bind2nd(std::greater<int>(), 42)};
-
-   // Use the unary function greater_than_42 with the std::partition algorithm.
    std::partition(std::begin(numbers), std::end(numbers), greater_than_42);
 
-   std::cout << "After partitioning:  ";
-   Print(numbers);
-   std::cout << std::endl;
+   Print("After partitioning:  ", numbers);
 
    return 0;
 }
